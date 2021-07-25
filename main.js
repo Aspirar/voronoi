ef0db4480fc381b8f0cf419790308fe07e1bb39e
Lakshya Gupta <theultimateaim@gmail.com>
2021-07-25 12:04:51 +0530

    Add files

---
+++ main.js
@@ -0,0 +1,179 @@
+const picker = document.getElementById('imagepicker');
+const preview = document.getElementById('preview');
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('webgl2');
+
+picker.addEventListener('change', (event) => {
+  preview.onload = () => {
+    canvas.width = preview.width;
+    canvas.height = preview.height;
+    draw(ctx);
+  };
+  preview.src = URL.createObjectURL(event.target.files[0]);
+});
+
+function getVShaderSrc() {
+  return `#version 300 es
+    in vec4 aPos;
+    in vec2 aTex;
+    out vec2 vTex;
+    out vec4 vPos;
+
+    void main() {
+      gl_Position = aPos;
+      vTex = aTex;
+      vPos = aPos;
+    }
+  `;
+}
+
+function getFShaderSrc() {
+  return `#version 300 es
+    precision highp float;
+    uniform sampler2D uImage;
+
+    in vec2 vTex;
+    in vec4 vPos;
+    out vec4 pixel;
+
+    vec2 randomV2(float seed) {
+      return vec2(fract(sin(seed * 2345.678)), fract(sin(seed * 38859.234)));
+    }
+
+    void main() {
+      vec2 uv = vPos.xy;
+
+      float minDist = 100.;
+      vec2 tex = vec2(0.);
+      for (float i = 0.; i < 5000.; i++) {
+        vec2 point = randomV2(i) * 2. - 1.;
+        float d = length(uv - point);
+        if (d < minDist) {
+          minDist = d;
+          tex = point;
+        }
+      }
+      vec3 color = vec3(minDist);
+      pixel = vec4(color, 1.);
+      pixel = texture(uImage, tex * vec2(.5, -.5) + .5);
+    }
+  `;
+}
+
+function createShader(gl, type, src) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, src);
+  gl.compileShader(shader);
+
+  if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+function createProgram(gl) {
+  const vShader = createShader(gl, gl.VERTEX_SHADER, getVShaderSrc());
+  const fShader = createShader(gl, gl.FRAGMENT_SHADER, getFShaderSrc());
+  const program = gl.createProgram();
+  gl.attachShader(program, vShader);
+  gl.attachShader(program, fShader);
+  gl.linkProgram(program);
+
+  if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+function getLocs(gl, program) {
+  return {
+    aPos: gl.getAttribLocation(program, 'aPos'),
+    aTex: gl.getAttribLocation(program, 'aTex'),
+    uImage: gl.getUniformLocation(program, 'uImage'),
+  };
+}
+
+function setVertexArray(gl) {
+  const vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+}
+
+function bufferAttrib(gl, loc, data) {
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+  gl.enableVertexAttribArray(loc);
+  gl.vertexAttribPointer(
+    loc,
+    2,
+    gl.FLOAT,
+    false,
+    0,
+    0,
+  );
+}
+
+function bufferPosAttrib(gl, locs) {
+  bufferAttrib(gl, locs.aPos, [
+    -1, -1,
+    -1, 1,
+    1, -1,
+    1, -1,
+    -1, 1,
+    1, 1,
+  ]);
+}
+
+function bufferTexAttrib(gl, locs) {
+  bufferAttrib(gl, locs.aTex, [
+    0, 1,
+    0, 0,
+    1, 1,
+    1, 1,
+    0, 0,
+    1, 0,
+  ]);
+}
+
+function bufferImage(gl) {
+  const texture = gl.createTexture();
+  gl.activeTexture(gl.TEXTURE0 + 0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    preview,
+  )
+}
+
+function initCanvas(gl) {
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function draw(gl) {
+  const program = createProgram(gl);
+  const locs = getLocs(gl, program);
+  setVertexArray(gl);
+  bufferPosAttrib(gl, locs);
+  bufferTexAttrib(gl, locs);
+  bufferImage(gl);
+  initCanvas(gl);
+  gl.useProgram(program);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
